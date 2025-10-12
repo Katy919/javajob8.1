efed2321c72e1ee66fd0b3a35ad85df9b874aff4
Ekaterina Golubeva <ekaterinagolybeva94@gmail.com>
2025-10-12 15:16:11 +0300

    Initial commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,8 +1,8 @@
 package ru.netology;
 
 public class Radio {
-    public int currentRadio;
-    public int currentVolume;
+    private int currentRadio;
+    private int currentVolume;
 
 
     public int getCurrentRadio() {
